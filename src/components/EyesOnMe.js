8a3ef82a5f915ac1abcd1c854ea1d7868b113339
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-02 11:17:55 +0300

    Updates

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    function handleFocus (e) {
+        console.log('Good!')
+    }
+    function handleBlur (e) {
+        console.log('Hey! Eyes on me!')
+    }
+  return (
+    <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
